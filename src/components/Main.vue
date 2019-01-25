a6dd9d1bbe35d55c95e69d83f411c9f7241af79a
Kelvin Au <kelvinau4413@hotmail.com>
2019-01-24 16:13:48 -0800

    add timeline with mock data

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     Main Component
+    <Timeline/>
   </div>
 </template>
 
 <script>
+import Timeline from '@/components/timeline';
+
 export default {
   name: 'Main',
+  components: {
+    Timeline,
+  },
   props: {
   },
 };
